fe24b56a7029092bd14ec7cc12834a0f05562482
Andrew Qu <qual1337@gmail.com>
2026-03-10 16:45:31 -0700

    /docs

--- docs-site/app/[lang]/(docs)/layout.tsx
+++ docs-site/app/[lang]/(docs)/layout.tsx
@@ -1,7 +1,7 @@
 import { DocsLayout } from "@/components/geistdocs/docs-layout";
 import { source } from "@/lib/geistdocs/source";
 
-const Layout = async ({ children, params }: LayoutProps<"/[lang]/docs">) => {
+const Layout = async ({ children, params }: LayoutProps<"/[lang]">) => {
   const { lang } = await params;
 
   return <DocsLayout tree={source.pageTree[lang]}>{children}</DocsLayout>;
